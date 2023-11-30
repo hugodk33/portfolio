b0ffef14b9c1027e13d808909bd1dd10d6d85bfd
Hugo <hugodk33@gmail.com>
2023-11-30 10:18:23 -0300

    'hehe'

--- src/App.js
+++ src/App.js
@@ -13,17 +13,18 @@ import { SiCsharp, SiExpress, SiDotnet, SiNestjs, SiAdonisjs, SiMysql, SiAmazona
 import { FaNodeJs } from 'react-icons/fa'
 import { DiPhotoshop, DiIllustrator } from 'react-icons/di'
 import { FiFigma } from 'react-icons/fi'
-import { TbBrandNextjs } from 'react-icons/tb'
+import { TbBrandNextjs, TbWorldWww } from 'react-icons/tb'
 import { GoAlert } from "react-icons/go";
 
 import careerRemix from './careerRemix.json'
+import career from './career.json'
 import knight from './assets/knight.png'
 import logoknight from './assets/logo/logo.svg'
 
 function App() {
 
-  const [timeLine, ] = useState(careerRemix)
-  const [actualTime, setActualTime] = useState(5)
+  const [timeLine,] = useState(career)
+  const [actualTime, setActualTime] = useState(timeLine.carrer.length - 1)
   const [actualService, setActualService] = useState(0)
 
   function changeTimeLine(value) {
@@ -50,7 +51,6 @@ function App() {
           </React.Fragment>
         );
       }
-
     });
   }
 
@@ -60,6 +60,7 @@ function App() {
     css: <BsFiletypeCss className='vrd-2' />,
     php: <BiLogoPhp className='vrd-2' />,
     javascript: <BiLogoJavascript className='vrd-2' />,
+    typescript: <BiLogoTypescript className='vrd-2' />,
     jquery: <SiJquery className='vrd-2' />,
     tailwind: <BiLogoTailwindCss className='vrd-2' />,
     node: <FaNodeJs className='vrd-2' />,
@@ -74,8 +75,11 @@ function App() {
   };
 
   useEffect(() => {
+    console.log('timeLine')
+    console.log(timeLine)
+    console.log('actualTime')
+    console.log(actualTime)
 
-    
   }, [timeLine])
 
 
@@ -88,7 +92,7 @@ function App() {
           <div className="flex items-center justify-center mr-auto ml-auto" style={{ maxWidth: '600px' }}>
             <div className="flex justify-center items-center flex-col p-5 rounded-md">
               <img id='knight' src={logoknight} />
-              <h1 className="bg-opacity-70 bg-black p-3 borded mr-auto ml-auto w-full mt-1 mb-1 bg-transparency-2 text-3xl text-white rounded-md" style={{ fontSize: '2.575rem' }}>
+              <h1 className="bg-opacity-70 bg-black p-3 borded mr-auto ml-auto w-full mt-1 mb-1 bg-transparency-2 text-3xl text-white rounded-md" style={{ fontSize: '1.7rem' }}>
                 Victor Hugo Amorim Arruda
               </h1>
               <span className='bg-black bg-opacity-50 mt-1 mb-1 pr-4 pl-4 rounded-full'>
@@ -140,7 +144,7 @@ function App() {
                 <p className="text-sm text-gray-400 pt-2 mb-2 max-w-xl">Profisional desenvolvedor desde <b>2019</b>, especialista na
                   criação de sites, <b>templates</b> e <b>plugins</b> para <b>wordpress</b>, aplicativos web em frameworks baseados
                   em <b>NodeJS</b>, atuando paralelamente como desenvolvedor na tecnologia <b>C#</b>.</p>
-                <div className='flex flex-row gap-4'>
+                <div className='flex flex-wrap gap-4'>
                   <div className='flex flex-col gap-2'>
                     <label className='text-sm flex mb-1 text-white w-full azl'>primary stack</label>
                     <hr className="w-full" />
@@ -167,7 +171,7 @@ function App() {
                   </div>
                 </div>
                 <label className='text-sm mb-5 azl w-full'>skills</label>
-                <div className='flex flex-row gap-4'>
+                <div className='flex flex-wrap gap-4'>
                   <div className='flex flex-col gap-2'>
                     <label className='vrd-2 w-full' style={{ fontSize: 9 }}>
                       back end
@@ -229,24 +233,21 @@ function App() {
               <div className='h-1 w-full flex bg-white mt-9' style={{ height: 2 }} />
               <div />
             </div>
-            <span className='flex w-full mt-5 mb-3 mr-auto ml-auto' style={{ maxWidth: 300, marginTop: 70 }}>
+            <span className='flex w-full mt-5 mb-3 mr-auto ml-auto' style={{ maxWidth: 180, marginTop: 70 }}>
               <ol id="steppter" className="flex items-center w-full mr-auto ml-auto" style={{ maxWidth: 300 }}>
                 {
                   timeLine.carrer.map((a, b) => {
                     switch (b) {
                       case 0:
                       case 1:
-                      case 2:
-                      case 3:
-                      case 4:
                         return (
                           <React.Fragment key={b}>
                             <li className="flex w-full items-center" onClick={() => changeTimeLine(b)}>
-                            <span className={"flex items-center justify-center w-8 h-8 bg-gray-300 rounded-full shrink-0" + (b < actualTime ? ' bg-green-300 ' : b === actualTime ? ' bg-gradient-to-b from-cyan-500 to-blue-700 border-white border-2 ' : ' bg-gray-200')}>
-                                <span className={"font-bold" + (b < actualTime ? ' text-blue-700 ' : b === actualTime ? ' text-white font-bolder' : ' text-blue-800 ')}>{a.year}</span>
+                              <span className={"flex items-center justify-center w-8 h-8 bg-gray-300 rounded-full shrink-0" + (b < actualTime ? ' bg-green-300 ' : b === actualTime ? ' bg-gradient-to-b from-cyan-500 to-blue-700 border-white border-2 ' : ' bg-gray-200')}>
+                                <span className={"font-bold" + (b < actualTime ? ' text-blue-700 ' : b === actualTime ? ' text-white font-bolder' : ' text-blue-800 ')} style={{ lineHeight: 1 }}>{a.year}</span>
                               </span>
                             </li>
-                            <hr className={'w-full border-2' + (b < actualTime ? ' border-green-300 ' : b === actualTime ? ' border-white' : ' border-gray-200 ')}/>
+                            <hr className={'w-full border-2' + (b < actualTime ? ' border-green-300 ' : b === actualTime ? ' border-white' : ' border-gray-200 ')} />
                           </React.Fragment>
                         );
                       default:
@@ -266,13 +267,13 @@ function App() {
               </ol>
             </span>
             <div className='flex w-full mt-2 mb-2 mr-auto ml-auto gap-3 justify-center' style={{ maxWidth: 300 }}>
-                {
-                  timeLine.carrer[actualTime].services.map((a , b) => {  
-                    return (
-                      <button className={"outline outline-offset-2 outline-1 border-gray-300 font-semibold w-5 h-5 rounded-full text-gray-300" + (b === actualService? ' bg-blue-500 ' : ' ')} onClick={() => setActualService(b)}>{ b + 1 }</button>
-                    )
-                  })  
-                }
+              {
+                timeLine.carrer[actualTime].services.map((a, b) => {
+                  return (
+                    <button className={"outline outline-offset-2 outline-1 border-gray-300 font-semibold w-5 h-5 rounded-full text-gray-300" + (b === actualService ? ' bg-blue-500 ' : ' ')} onClick={() => setActualService(b)}>{b + 1}</button>
+                  )
+                })
+              }
             </div>
             <div className="md:w-2/3 white text-left pt-2 pb-3 mr-auto ml-auto" style={{ maxWidth: 650 }}>
               <hr className='w-full mt-1 mb-3 vrd' />
@@ -282,19 +283,32 @@ function App() {
                 {generateTechOutput(timeLine.carrer[actualTime].services[actualService].techs)}
               </p>
               <hr className='w-full mt-3 mb-1 vrd' />
-              { 
-                timeLine.carrer[actualTime].services[actualService].challenge?
-                <>
-                  <label className='text-blue-300 mt-1 uppercase' style={{fontSize: 9}}>desafio:</label>
-                  <div class="flex items-center azl-bg text-white text-sm font-bold px-4 py-3 mt-1 mb-2" role="alert">
-                    <GoAlert className='text-white text-2xl mr-2'/>
-                    <p>{timeLine.carrer[actualTime].services[actualService].challenge}</p>
-                  </div>
-                </>
-                :null
+              {
+                timeLine.carrer[actualTime].services[actualService].challenge ?
+                  <>
+                    <label className='text-blue-300 mt-1 uppercase' style={{ fontSize: 9 }}>desafio:</label>
+                    <div class="flex items-center azl-bg text-white text-sm font-bold px-4 py-3 mt-1 mb-2" role="alert">
+                      <GoAlert className='text-white text-2xl mr-2' />
+                      <p>{timeLine.carrer[actualTime].services[actualService].challenge}</p>
+                    </div>
+                  </>
+                  : null
               }
-              <label className='text-blue-300 mt-1 uppercase' style={{fontSize: 9}}>descrição:</label>
+              <label className='text-blue-300 mt-1 uppercase' style={{ fontSize: 9 }}>descrição:</label>
               <div className="text-gray-200">{transformarQuebraDeLinhaEmParagrafo(timeLine.carrer[actualTime].services[actualService].description)}</div>
+              {
+                timeLine.carrer[actualTime].services[actualService].link ?
+                  <a
+                    href={timeLine.carrer[actualTime].services[actualService].link}
+                    target="_blank" rel="noopener noreferrer"
+                    className='flex flex-row mgnt-bg2 py-2 mt-2 px-5 rounded-full'
+                    style={{ maxWidth: '130px'}}
+                  >
+                    <TbWorldWww className='mr-2 text-xl' style={{ marginTop: '2px'}} />
+                    ver site.
+                  </a>
+                  : null
+              }
             </div>
           </div>
         </Sessao>
